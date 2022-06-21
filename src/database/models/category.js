5faf10ec8c3768e413226f14728a4922ebc290a8
¨Venturin¨ <diegoventurinn@gmail.com>
2022-06-21 19:11:32 -0300

    feat: created sequelize model "Category"

---
+++ src/database/models/category.js
@@ -0,0 +1,20 @@
+const Category = (sequelize, DataTypes) => {
+  const Category = sequelize.define(
+    "Category",
+    {
+      id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+      name: DataTypes.STRING,
+    },
+    {
+      timestamps: false,
+      tableName: "Category",
+      underscored: false,
+    }
+  );
+
+  return Category;
+};
+
+module.exports = Category;
+
+
