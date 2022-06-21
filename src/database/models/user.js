63c24d7d82824877a5b2dd333c98f7fccc3470f6
¨Venturin¨ <diegoventurinn@gmail.com>
2022-06-21 11:26:17 -0300

    feat: created sequelize model "User"

---
+++ src/database/models/user.js
@@ -0,0 +1,22 @@
+const User = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    "User",
+    {
+      id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+      displayName: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      image: DataTypes.STRING,
+    },
+
+    { timestamps: false, tableName: "Users", underscored: true }
+  );
+
+  User.associate = (models) => {
+    User.hasMany(models.blogPost, { foreignKey: "userId", as: "blogPosts" });
+  };
+
+  return User;
+};
+
+module.exports = User;
