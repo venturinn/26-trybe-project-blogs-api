09d1bd4c120963d2c50fb6f3c27d21df00fab1a0
¨Venturin¨ <diegoventurinn@gmail.com>
2022-06-22 11:22:53 -0300

    feat: created sequelize model "PostCategory"

---
+++ src/database/models/postCategory.js
@@ -0,0 +1,26 @@
+const PostCategory = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define(
+    "PostCategory",
+    {
+      postId: { type: DataTypes.INTEGER, foreignKey: true },
+      categoryId: { type: DataTypes.INTEGER, foreignKey: true },
+    },
+    { timestamps: false, tableName: "PostCategories", underscored: false }
+  );
+  PostCategory.associate = (models) => {
+    models.Category.belongsToMany(models.BlogPost, {
+      as: "blogPosts",
+      through: PostCategory,
+      foreignKey: "categoryId",
+      otherKey: "postId",
+    });
+    models.BlogPost.belongsToMany(models.Category, {
+      as: "categories",
+      through: PostCategory,
+      foreignKey: "postId",
+      otherKey: "categoryId",
+    });
+  };
+  return PostCategory;
+};
+module.exports = PostCategory;
