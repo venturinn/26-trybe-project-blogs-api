85d25f774d7346e1bcb278da8314d24d3fde7c9e
¨Venturin¨ <diegoventurinn@gmail.com>
2022-07-04 12:22:46 -0300

    refactor: changed database MySQL to Postgres

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,32 +1,30 @@
-require('dotenv').config();
+require('dotenv/config');
 
-const environment = process.env.NODE_ENV || 'test';
-
-const suffix = {
-  dev: '-dev',
-  development: '-dev',
-  test: '-test',
-};
-
-const options = {
-  host: process.env.HOSTNAME || process.env.MYSQL_HOST || 'localhost',
-  port: process.env.MYSQL_PORT || '3306',
-  database: 
-    `${process.env.MYSQL_DB_NAME || 'blogs-api'}${suffix[environment] || suffix.test}`,
-  username: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || '1234',
-  dialect: 'mysql',
-  dialectOptions: {
-    timezone: 'Z',
-  },
-  logging: process.env.DEBUG !== 'false',
-};
+const { HOST, PASSWORD_POSTGRES, DATABASE, DB_USERNAME, DB_PORT } = process.env;
 
 module.exports = {
   development: {
-    ...options,
+    username: DB_USERNAME,
+    password: PASSWORD_POSTGRES,
+    database: DATABASE,
+    host: HOST,
+    port: DB_PORT,
+    dialect: 'postgres',
   },
   test: {
-    ...options,
+    username: DB_USERNAME,
+    password: PASSWORD_POSTGRES,
+    database: DATABASE,
+    host: HOST,
+    port: DB_PORT,
+    dialect: 'postgres',
+  },
+  production: {
+    username: DB_USERNAME,
+    password: PASSWORD_POSTGRES,
+    database: DATABASE,
+    host: HOST,
+    port: DB_PORT,
+    dialect: 'postgres',
   },
 };
